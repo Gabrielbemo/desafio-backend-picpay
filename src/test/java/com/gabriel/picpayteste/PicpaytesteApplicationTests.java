f3d2774c827cee5edf36d0be8096276ee063dc13
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-14 19:30:27 -0300

    first commit

---
+++ src/test/java/com/gabriel/picpayteste/PicpaytesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.picpayteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaytesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
