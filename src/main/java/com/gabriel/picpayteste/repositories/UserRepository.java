8d7755ae20c5fb52b16c830e524a28a0680a86ac
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-15 19:21:01 -0300

    adding UserRepository and config applications properties

---
+++ src/main/java/com/gabriel/picpayteste/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gabriel.picpayteste.repositories;
+
+import com.gabriel.picpayteste.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findById(Long id);
+}
