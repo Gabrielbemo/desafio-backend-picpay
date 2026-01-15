becf4414ddc805b17cc4e8df92fbf7757447e6f1
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-15 20:03:59 -0300

    adding services and fix the lombook in pom.xml

---
+++ src/main/java/com/gabriel/picpayteste/services/UserService.java
@@ -0,0 +1,35 @@
+package com.gabriel.picpayteste.services;
+
+import com.gabriel.picpayteste.domain.user.User;
+import com.gabriel.picpayteste.domain.user.UserType;
+import com.gabriel.picpayteste.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    private UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public void validationTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Sem autorização para fazer transação");
+        }
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findById(id).orElseThrow(() -> new Exception("Usuario nao encontrado"));
+    }
+
+    public void saveUSer(User user){
+        this.repository.save(user);
+    }
+}
